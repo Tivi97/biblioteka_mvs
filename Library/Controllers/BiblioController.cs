97abc60943f66d7b9403dd42a818bc1e0c8050f0
Tivi97 <34186593+Tivi97@users.noreply.github.com>
2018-03-01 02:02:42 +0400

    Добавьте файлы проекта.
    первая страница

---
+++ Library/Controllers/BiblioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BiblioController : Controller
+    {
+        // GET: Biblio
+        public ActionResult Biblio()
+        {
+            return View();
+        }
+    }
+}
